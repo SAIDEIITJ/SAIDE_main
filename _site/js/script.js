69dd1e4cb1154bbbdf186b5761d92e7eeeb56e1c
Satvik Wazir <119810814+sw509073@users.noreply.github.com>
2024-06-07 17:56:19 +0530

    changed landing page

--- _site/js/script.js
+++ _site/js/script.js
@@ -5,18 +5,18 @@ function getRandomColor() {
 
 $(document).ready(function() {
     const backgroundImages = [
-        'lamp.jpg',
-        'book.jpg',
-        'sk2.jpg',
-        'mission.jpg',
-        'sk6.jpg', // Change to your image paths 
+        'bg.png',
+        'Programs.png',
+        'ar.png',
+        'ms.png',
+        'btech.png', // Change to your image paths 
     ];
 
     let currentIndex = 0;
     function changeBackground() {
         currentIndex = (currentIndex + 1) % backgroundImages.length;
         // const randomColor = getRandomColor();
-        $('.background').css('background-image', `url(images/${backgroundImages[currentIndex]})`);
+        // $('.background').css('background-image', `url(images/${backgroundImages[currentIndex]})`);
         // $('.background').css('background-color',randomColor);
         $('.rectangular-div').css('background-image', `url(images/${backgroundImages[currentIndex]})`);
     }
