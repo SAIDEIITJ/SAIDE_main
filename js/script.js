539e008f03debecfdf39aae0823454f0f4b52356
Satvik Wazir <119810814+sw509073@users.noreply.github.com>
2024-06-20 20:01:43 +0530

    Changes:event and news4

--- js/script.js
+++ js/script.js
@@ -66,21 +66,22 @@ window.onclick = function(event) {
     }
 }
 
- const newsData = [
 
-    { id: "news-card-1", title: "Selected Candidates for PhD Program (2024–25 Semester-I, July 2024)", date: "JULY, 2024", content: "Gakjs akljshd coia aklsuhd ausdc asoich auosy saou...", img: "images/news2.png" },
-    { id: "news-card-2", title: "Selected Candidates for PhD Program in CMCE (2024–25 Semester-I, July 2024)", date: "JULY, 2024", content: "Guah ssuhd a usdc asoich aalsih  saou...", img: "images/news5.png" },
-    { id: "news-card-3", title: "Selected Candidates for MS by Research Program in CMCE (2024–25 Semester-I, July 2024)", date: "JULY, 2024", content: "Guah ssuhd a usdc asoich aalsih  saou...", img: "images/news3.png" },
-    { id: "news-card-4", title: "Selected Candidates for MS by Research Program in CII (2024–25 Semester-I, July 2024)", date: "JULY, 2024", content: "Gakjs akljshd coia aklsuhd ausdc asoich auosy saou...", img: "images/news2.png" },
-    { id: "news-card-5", title: "Indicative projects for upcoming PhD admission cycle, Sem-I, 2024-2025", date: "JULY, 2024", content: "Guah ssuhd a usdc asoich aalsih  saou...", img: "images/news5.png" },
-    { id: "news-card-6", title: "Selected Candidates for Summer Internship Program 2024", date: "JULY, 2024", content: "Guah ssuhd a usdc asoich aalsih  saou...", img: "images/news3.png" },
-    { id: "news-card-7", title: "Rolling Advertisement for Admission to Ph.D. Program at CMCE, AIDE, IITJ", date: "JULY, 2024", content: "Gakjs akljshd coia aklsuhd ausdc asoich auosy saou...", img: "images/news2.png" },
-    { id: "news-card-8", title: "Rolling Advertisement for Admission to Ph.D. Program", date: "MAY, 2024", content: "Mas asjd cioue c asu coausc clow dcwl sdcoua couc el...", img: "images/news4.png" },
-    { id: "news-card-9", title: "Rolling Advertisement for Faculty Hiring", date: "JULY, 2024", content: "Guah ssuhd a usdc asoich aalsih  saou...", img: "images/news3.png" },
+//  const newsData = [
 
-    // Add more news items here
-];
+//     { id: "news-card-1", title: "Selected Candidates for PhD Program (2024–25 Semester-I, July 2024)", date: "JULY, 2024", content: "Gakjs akljshd coia aklsuhd ausdc asoich auosy saou...", img: "images/news2.png" },
+//     { id: "news-card-2", title: "Selected Candidates for PhD Program in CMCE (2024–25 Semester-I, July 2024)", date: "JULY, 2024", content: "Guah ssuhd a usdc asoich aalsih  saou...", img: "images/news5.png" },
+//     { id: "news-card-3", title: "Selected Candidates for MS by Research Program in CMCE (2024–25 Semester-I, July 2024)", date: "JULY, 2024", content: "Guah ssuhd a usdc asoich aalsih  saou...", img: "images/news3.png" },
+//     { id: "news-card-4", title: "Selected Candidates for MS by Research Program in CII (2024–25 Semester-I, July 2024)", date: "JULY, 2024", content: "Gakjs akljshd coia aklsuhd ausdc asoich auosy saou...", img: "images/news2.png" },
+//     { id: "news-card-5", title: "Indicative projects for upcoming PhD admission cycle, Sem-I, 2024-2025", date: "JULY, 2024", content: "Guah ssuhd a usdc asoich aalsih  saou...", img: "images/news5.png" },
+//     { id: "news-card-6", title: "Selected Candidates for Summer Internship Program 2024", date: "JULY, 2024", content: "Guah ssuhd a usdc asoich aalsih  saou...", img: "images/news3.png" },
+//     { id: "news-card-7", title: "Rolling Advertisement for Admission to Ph.D. Program at CMCE, AIDE, IITJ", date: "JULY, 2024", content: "Gakjs akljshd coia aklsuhd ausdc asoich auosy saou...", img: "images/news2.png" },
+//     { id: "news-card-8", title: "Rolling Advertisement for Admission to Ph.D. Program", date: "MAY, 2024", content: "Mas asjd cioue c asu coausc clow dcwl sdcoua couc el...", img: "images/news4.png" },
+//     { id: "news-card-9", title: "Rolling Advertisement for Faculty Hiring", date: "JULY, 2024", content: "Guah ssuhd a usdc asoich aalsih  saou...", img: "images/news3.png" },
 
+//     // Add more news items here
+// ];
+console.log(newsData);
 
 // Define an object for storing events
  const eventData = [
