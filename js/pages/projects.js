ebca269f8c22ba98fa1e2bacdf8258c09df36f0d
Satvik Wazir <119810814+sw509073@users.noreply.github.com>
2024-07-30 17:07:25 +0530

    status of projects

--- js/pages/projects.js
+++ js/pages/projects.js
@@ -1,9 +1,35 @@
 document.addEventListener('DOMContentLoaded', function() {
   const projectItems = document.querySelectorAll('.project-item');
+  const today = new Date(); // Get today's date
 
   projectItems.forEach(item => {
+      const index = item.dataset.index;
+      const endDateText = item.querySelector('#popup-dates').textContent.split(' - ')[1];
+      const endDateParts = endDateText.split('-');
+      const endDate = new Date(endDateParts[2], endDateParts[1] - 1, endDateParts[0]); // Parse the end date
+
+      // Determine the status based on the end date
+      const status = today > endDate ? 'Completed' : 'Ongoing';
+
+      // Find the status element and update its content
+      const statusElement = item.querySelector('#popup-status');
+
+
+      if (status == 'Completed'){
+          if (statusElement) {
+            statusElement.innerHTML = `<strong>Status:<i class="fa-solid fa-check" style="color: #63E6BE;"></i></strong> ${status}`;
+        }
+      }
+      else {
+          if (statusElement) {
+            statusElement.innerHTML = `<strong>Status:<i class="fa-solid fa-circle" style="color: #FFD43B;"></i></strong> ${status}`;
+        }
+
+      }
+    
+
+      // Add event listener to toggle project details
       item.addEventListener('click', function() {
-          const index = this.dataset.index;
           const details = document.getElementById(`project-details-${index}`);
           
           // Toggle display of the details
